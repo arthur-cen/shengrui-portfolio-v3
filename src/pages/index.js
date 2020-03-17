4fb19cd40c643ec1f45b8e3f5b87627bbe51e34b
Shengrui Cen <cenrui.93@gmail.com>
2020-03-16 22:33:56 -0500

    removed unnecessary imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
 import React from "react"
-import { createMemoryHistory } from "history"
-
 import "assets/scss/material-kit-react.scss?v=1.4.0"
 import "typeface-roboto"
 import "typeface-roboto-slab"
 // pages for this product
 import Components from "../pages-components/Components/Components.jsx"
-
-let hist = createMemoryHistory()
-
 export default () => (<Components/>)
